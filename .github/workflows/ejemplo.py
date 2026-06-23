9b03f85f5c2a0c0780dcbc80c51671672b987e93
German818181 <ramiromont2018@gmail.com>
2026-06-23 18:55:17 -0300

    Add function to check if a number is prime

--- .github/workflows/ejemplo.py
+++ .github/workflows/ejemplo.py
@@ -29,3 +29,14 @@ def buscar_elemento(lista, elemento):
         return posicion
     else:
         return -1
+
+
+
+def es_primo(numero):
+    if numero < 2:
+        return False
+    divisores = []
+    for i in range(1, numero + 1):
+        if numero % i == 0:
+            divisores.append(i)
+    return len(divisores) == 2
