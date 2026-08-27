ecfa0d25bf41747773ee71253b0ad9771dd35fed
German818181 <ramiromont2018@gmail.com>
2026-08-27 19:03:14 -0300

    Change total records in risk simulator from 100 to 50

--- riesgo_financiero.py
+++ riesgo_financiero.py
@@ -63,7 +63,7 @@ class SimuladorRiesgoFinanciero:
         return buffer_texto
 
 def ejecutar_motor_pesado():
-    motor = SimuladorRiesgoFinanciero(registros_totales=100)
+    motor = SimuladorRiesgoFinanciero(registros_totales=50)
     fraudulentos = motor.cruzar_con_listas_negras()
     reporte = motor.generar_reporte_masivo_texto()
     triplas = motor.detectar_patrones_lavado_triples()
